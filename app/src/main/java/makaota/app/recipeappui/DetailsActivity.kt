1d438e258f320a9bc1995cc5449ccf63f3d25beb
makaota <74915165+makaota@users.noreply.github.com>
2022-06-25 15:35:09 +0200

    details screen ingredient section and Button section

--- app/src/main/java/makaota/app/recipeappui/DetailsActivity.kt
+++ app/src/main/java/makaota/app/recipeappui/DetailsActivity.kt
@@ -1,5 +1,6 @@
 package makaota.app.recipeappui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,7 +12,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -20,6 +26,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import makaota.app.recipeappui.ui.theme.Linen
 import makaota.app.recipeappui.ui.theme.RecipeAppUITheme
 
@@ -37,6 +44,9 @@ class DetailsActivity : ComponentActivity() {
                 Column {
                     TopSection()
                     ImageSection()
+                    IngredientsAndServingSection()
+                    IngredientsSection()
+                    ButtonSection()
 
                 }
 
@@ -89,60 +99,60 @@ fun TopSection() {
 @Composable
 fun ImageSection() {
 
-       Row(
-           horizontalArrangement = Arrangement.SpaceEvenly,
-           verticalAlignment = Alignment.CenterVertically,
-           modifier = Modifier
-               .fillMaxWidth()
-               .padding(15.dp)
-       ) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
 
-           Card(
-               shape = RoundedCornerShape(10.dp),
-               elevation = 5.dp
-           ) {
+        Card(
+            shape = RoundedCornerShape(10.dp),
+            elevation = 5.dp
+        ) {
 
-               Image(
-                   painter = painterResource(id = R.drawable.grilled_salmon_sushi_roll_sauce),
-                   contentDescription = "",
-                   contentScale = ContentScale.Crop
-               )
+            Image(
+                painter = painterResource(id = R.drawable.grilled_salmon_sushi_roll_sauce),
+                contentDescription = "",
+                contentScale = ContentScale.Crop
+            )
 
 
-               Column(
-                   verticalArrangement = Arrangement.Center,
-                   modifier = Modifier.padding(15.dp)
-               ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.padding(15.dp)
+            ) {
 
-                   Card(Modifier.size(50.dp), RoundedCornerShape(10.dp)) {
+                Card(Modifier.size(50.dp), RoundedCornerShape(10.dp)) {
 
-                       Box(
-                           Modifier
-                               .fillMaxSize()
-                               .background(
-                                   Brush.verticalGradient(
-                                       colors = listOf(
-                                           Color.Transparent,
-                                           Color.Black
-                                       ), startY = 299f, 30f
-                                   )
-                               )
-                       )
+                    Box(
+                        Modifier
+                            .fillMaxSize()
+                            .background(
+                                Brush.verticalGradient(
+                                    colors = listOf(
+                                        Color.Transparent,
+                                        Color.Black
+                                    ), startY = 299f, 30f
+                                )
+                            )
+                    )
 
-                       Image(
-                           painter = painterResource(id = R.drawable.ic_turned_in),
-                           contentDescription = "",
-                           contentScale = ContentScale.Crop,
-                           modifier = Modifier.clickable {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_turned_in),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.clickable {
 
-                           }
-                       )
+                        }
+                    )
 
-                   }
-               }
-           }
+                }
+            }
+        }
 
-       }
+    }
 
 }
 
@@ -150,5 +160,186 @@ fun ImageSection() {
 fun IngredientsAndServingSection() {
 
 
+    var title = "Ingredients (12)"
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+
+        Column(verticalArrangement = Arrangement.Center) {
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.body1
+            )
+
+        }
+
+        ///////////////////////
+
+        var expanded by remember { mutableStateOf(false) }
+        val suggestions = listOf("Item1", "Item2", "Item3")
+
+        Box {
+            Button(onClick = { expanded = !expanded }) {
+                Text("DropDown")
+                Icon(
+                    imageVector = Icons.Filled.ArrowDropDown,
+                    contentDescription = null,
+                )
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+            ) {
+                suggestions.forEach { label ->
+                    DropdownMenuItem(onClick = {
+                        expanded = false
+                        //do something ...
+                    }) {
+                        Text(text = label)
+                    }
+                }
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun IngredientsSection() {
+
+    var title = "Matcha"
+    var item = "1 teaspoon"
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.matcha),
+            contentDescription = "matcha",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .width(50.dp)
+                .height(40.dp)
+                .clickable {
+
+                }
+        )
+        Column(verticalArrangement = Arrangement.Center) {
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.body1
+            )
+
+        }
+        Text(
+            text = item,
+            style = MaterialTheme.typography.body1
+        )
+
+    }
+
+    title = "Rice Bowl"
+    item = "200 gr"
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.rice),
+            contentDescription = "rice bowl",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .width(50.dp)
+                .height(40.dp)
+                .clickable {
+
+                }
+        )
+        Column(verticalArrangement = Arrangement.Center) {
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.body1
+            )
+
+        }
+
+
+        Text(
+            text = item,
+            style = MaterialTheme.typography.body1
+        )
+
+
+    }
+
+    title = "Salmon"
+    item = "400 gr"
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.salmon),
+            contentDescription = "salmon",
+            modifier = Modifier
+                .width(50.dp)
+                .height(40.dp)
+        )
+        Column(verticalArrangement = Arrangement.Center) {
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.body1
+            )
+
+        }
+
+        Text(
+            text = item,
+            style = MaterialTheme.typography.body1
+        )
+    }
+
+}
+
+@Composable
+fun ButtonSection(){
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Button(onClick = {
+
+        }) {
+            Text(text = "Navigate", fontSize = 18.sp)
+
+        }
+    }
 }
 
